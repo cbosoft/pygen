21f57e972c13a47f1b5aa14aea9afe532a0f2231
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-06-15 22:03:06 +0100

    Improve legibility of test

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from digits_of_pi import DIGITS_OF_PI
 
 
 def fail(i, digit_exp, digit_got):
-    print(f'Test failed at digit #{i} (expected {digit_exp}, got {digit_got}')
+    print(f'Test failed at digit #{i} (expected {digit_exp}, got {digit_got})')
     sys.exit(1)
 
 
@@ -14,7 +14,7 @@ def test():
 
     for i, digit in enumerate(DIGITS_OF_PI):
         digit_got = pg.get_next_digit()
-
+        print(digit, digit_got)
         if digit != digit_got:
             fail(i, digit, digit_got)
 
